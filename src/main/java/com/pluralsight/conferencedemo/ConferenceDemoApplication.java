053025db8deb42913dc3a1ac2c31794317e625be
Raj Singh <neeraj.singh.col@gmail.com>
2020-01-05 11:43:43 -0500

    udpated todo.txt and main java file

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConferenceDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting application....");
 		SpringApplication.run(ConferenceDemoApplication.class, args);
 	}
 
